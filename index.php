e5ffda56e854f23d4cbcfd8cb5f3bffd9646f1e0
Đàm <14832060+85mine@users.noreply.github.com>
2023-09-15 23:01:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,37 @@
 <?php
-header("Content-Type: audio/mpegurl;");
-if(isset($_GET['type']) && $_GET['type'] == 'epg'){
-  echo file_get_contents('http://lichphatsong.xyz/schedule/beartv_epg.xml');
-}
-else{
-  echo file_get_contents('http://gg.gg/bearlivetv');
+// Set the correct content type for the response
+header("Content-Type: audio/mpegurl");
+
+if (isset($_GET['type']) && $_GET['type'] === 'epg') {
+  // Use cURL to fetch the remote XML content
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, 'http://lichphatsong.xyz/schedule/beartv_epg.xml');
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $epg_content = curl_exec($ch);
+
+  if ($epg_content === false) {
+    // Handle error if cURL request fails
+    echo "Error fetching EPG data";
+  } else {
+    // Output the fetched EPG content
+    echo $epg_content;
+  }
+
+  curl_close($ch);
+} else {
+  // Use cURL to fetch the remote content
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, 'http://gg.gg/bearlivetv');
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $livetv_content = curl_exec($ch);
+
+  if ($livetv_content === false) {
+    // Handle error if cURL request fails
+    echo "Error fetching live TV data";
+  } else {
+    // Output the fetched live TV content
+    echo $livetv_content;
+  }
+
+  curl_close($ch);
 }
