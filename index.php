00b239f99164e6afe1192a8d17e343c1d0622ba8
Đàm <14832060+85mine@users.noreply.github.com>
2023-09-15 22:58:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ if(isset($_GET['type']) && $_GET['type'] == 'epg'){
   echo file_get_contents('http://lichphatsong.xyz/schedule/beartv_epg.xml');
 }
 else{
-  echo file_get_contents('i.m3u');
+  echo file_get_contents('http://gg.gg/bearlivetv');
 }
